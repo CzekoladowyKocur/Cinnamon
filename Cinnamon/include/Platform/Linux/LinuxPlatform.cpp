1202a61a7c15c10217d8d322f41a6dc88d2758bb
rcubee <kakapokakapo@protonmail.com>
2022-08-18 17:18:21 +0200

    linux support for colors in terminal output

--- Cinnamon/include/Platform/Linux/LinuxPlatform.cpp
+++ Cinnamon/include/Platform/Linux/LinuxPlatform.cpp
@@ -1,6 +1,26 @@
 #ifdef CIN_PLATFORM_LINUX
 #include "Cinnamon/include/Core/Core.h"
 
+constexpr char C_FOREGROUND_BLACK[]					{ "\033[0;30m" };
+constexpr char C_FOREGROUND_RED[]					{ "\033[0;31m" };
+constexpr char C_FOREGROUND_GREEN[]					{ "\033[0;32m" };
+constexpr char C_FOREGROUND_YELLOW[]				{ "\033[0;33m" };
+constexpr char C_FOREGROUND_BLUE[]					{ "\033[0;34m" };
+constexpr char C_FOREGROUND_MAGENTA[]				{ "\033[0;35m" };
+constexpr char C_FOREGROUND_CYAN[]					{ "\033[0;36m" };
+constexpr char C_FOREGROUND_WHITE[]					{ "\033[0;37m" };
+
+constexpr char C_FOREGROUND_BRIGHTBLACK[]			{ "\033[0;90m" };
+constexpr char C_FOREGROUND_BRIGHTRED[]				{ "\033[0;91m" };
+constexpr char C_FOREGROUND_BRIGHTGREEN[]			{ "\033[0;92m" };
+constexpr char C_FOREGROUND_BRIGHTYELLOW[]			{ "\033[0;93m" };
+constexpr char C_FOREGROUND_BRIGHTBLUE[]			{ "\033[0;94m" };
+constexpr char C_FOREGROUND_BRIGHTMAGENTA[]			{ "\033[0;95m" };
+constexpr char C_FOREGROUND_BRIGHTCYAN[]			{ "\033[0;96m" };
+constexpr char C_FOREGROUND_BRIGHTWHITE[]			{ "\033[0;97m" };
+
+constexpr char C_FOREGROUND_DEFAULT[]				{ "\033[0;0m" };
+
 namespace Cinnamon {
 
 	bool Platform::Initialize()
@@ -58,7 +78,17 @@ namespace Cinnamon {
 
 	void Platform::WriteToConsole(const char* message, const EConsoleTextColor color)
 	{
-        CIN_UNUSED(color); printf("%s", message);
+		/* Kinda reflected off EConsoleTextColor - Magenta is big no no */
+		constexpr const char* colors[5]
+		{
+			C_FOREGROUND_DEFAULT,
+			C_FOREGROUND_BRIGHTBLACK,
+			C_FOREGROUND_YELLOW,
+			C_FOREGROUND_BRIGHTRED,
+			C_FOREGROUND_RED
+		};
+
+		printf("%s%s%s", colors[static_cast<size_t>(color)], message, "\033[0m");
 	}
 
 	[[nodiscard]] STL::String Platform::GetBuildDate()
